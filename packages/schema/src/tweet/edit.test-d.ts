3da0997f3d972c2fd2c64dadab79e54d2ca5664f
ocavue <ocavue@gmail.com>
2026-09-09 20:15:29 +1000

    fix

--- packages/schema/src/tweet/edit.test-d.ts
+++ packages/schema/src/tweet/edit.test-d.ts
@@ -3,7 +3,7 @@ import { expectTypeOf, test } from 'vitest'
 
 import type { TweetEditControl } from '@post-embed/types/internal/tweet/edit'
 
-import type { TweetEditControlSchema } from './edit.ts' // FIXME: use ./edit.ts instead of ./edit.js
+import type { TweetEditControlSchema } from './edit.ts' // FIXME: use ./edit.ts instead of ./edit.js. apply this rule to all files under schema/src/
 
 test('TweetEditControlSchema', () => {
   expectTypeOf<v.InferOutput<typeof TweetEditControlSchema>>().toEqualTypeOf<TweetEditControl>()
