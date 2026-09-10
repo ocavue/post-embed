3eda7867e34b79910a1317274b2158ed92f32698
ocavue <ocavue@gmail.com>
2026-09-10 19:19:10 +1000

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,5 @@
 import { defineESLintConfig } from '@ocavue/eslint-config'
 
 export default defineESLintConfig(
-  { react: true },
-  {
-    files: ['packages/elements/src/x/x-post.ts'],
-    // aria-ui hooks manage mutable DOM hosts outside React.
-    rules: { 'react-hooks/immutability': 'off' },
-  },
+  { react: false, jsdoc: true, ocavue: true  },
 )
