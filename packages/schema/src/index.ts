d7dacb398f40f33ea9c3ef4401aa060d18bcfed3
ocavue <ocavue@gmail.com>
2026-09-10 18:57:07 +1000

    review

--- packages/schema/src/index.ts
+++ packages/schema/src/index.ts
@@ -4,10 +4,12 @@ import type { StandardSchemaV1 } from '@standard-schema/spec'
 import { EnrichedTweetSchema, TweetSchema } from './tweet/index.ts'
 
 export {
+  // FIXME: do NOT rename TweetSchema to tweetSchema. Just keep TweetSchema and EnrichedTweetSchema
   TweetSchema as tweetSchema,
   EnrichedTweetSchema as enrichedTweetSchema,
 }
 
+// 1. let's remove parseTweet and parseEnrichedTweet from this public API REVIEW FIXME
 // These schemas are synchronous; Valibot's Standard Schema signature also permits promises.
 export function parseTweet(input: unknown): StandardSchemaV1.Result<Tweet> {
   return TweetSchema['~standard'].validate(
