d03f2ccb5caf554de843cb4977316036e8491197
ocavue <ocavue@gmail.com>
2026-09-10 20:26:42 +1000

    fix

--- packages/elements/tsdown.config.ts
+++ packages/elements/tsdown.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'tsdown'
 
 export default defineConfig({
-  entry: { 'x/index': './src/x/index.ts' },
-  outDir: './dist',
-  dts: true,
-  copy: [{ from: './src/x/theme.css', to: './dist/x' }],
-  outputOptions: { entryFileNames: '[name].js' },
+  entry: ['./src/index.ts', './src/x/index.ts', ],
+  copy: [{
+    from: 'src/x/theme.css',
+    to: 'dist/x/',
+  }],
+  fixedExtension: false,
 })
