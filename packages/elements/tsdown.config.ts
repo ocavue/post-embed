f6962936a0bbbe7c102d96d6a93b93d11d3263c8
ocavue <ocavue@gmail.com>
2026-09-12 19:08:34 +1000

    feat(elements): add `@post-embed/elements/youtube`

--- packages/elements/tsdown.config.ts
+++ packages/elements/tsdown.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from 'tsdown'
 
 export default defineConfig({
-  entry: ['./src/index.ts', './src/x/index.ts'],
+  entry: ['./src/index.ts', './src/x/index.ts', './src/youtube/index.ts'],
   copy: [
     {
       from: 'src/x/theme.css',
       to: 'dist/x/',
+    },
+    {
+      from: 'src/youtube/theme.css',
+      to: 'dist/youtube/',
     },
   ],
   fixedExtension: false,
