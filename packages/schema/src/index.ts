32d3040fea8cb11963be93692e4a3cbf13b60e24
ocavue <ocavue@gmail.com>
2026-09-15 02:35:03 +1000

    chore: add third-round simplification FIXMEs

--- packages/schema/src/index.ts
+++ packages/schema/src/index.ts
@@ -5,5 +5,6 @@ export { YouTubeVideoSchema } from './youtube/index.ts'
 export { looseItems } from './primitives.ts'
 
 export { mapXPostMediaUrls } from './x/media.ts'
+// FIXME: two export statements from the same module; fold `XPostIdSchema` into this one.
 export { parseXPostId, X_POST_ID_PATTERN } from './x/url.ts'
 export { XPostIdSchema } from './x/url.ts'
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
+  // FIXME: pure reformat of an unrelated file; revert.
   test: { projects: ['./packages/*/vitest.config.ts'] },
 })
