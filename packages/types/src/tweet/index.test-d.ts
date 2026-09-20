f3ec7785f29a447bb8aefde800c602389391a3be
ocavue <ocavue@gmail.com>
2026-09-21 07:04:47 +1000

    test(types): simplify `Tweet` compatibility assertion

--- packages/types/src/tweet/index.test-d.ts
+++ packages/types/src/tweet/index.test-d.ts
@@ -3,14 +3,6 @@ import { expectTypeOf, test } from 'vitest'
 
 import type { Tweet } from './index.js'
 
-test('Tweet accepts react-tweet data and optional quote thread metadata', () => {
+test('Tweet accepts react-tweet data', () => {
   expectTypeOf<UpstreamTweet>().toExtend<Tweet>()
-  expectTypeOf<Omit<Tweet, 'quoted_tweet'>>().toEqualTypeOf<
-    Omit<UpstreamTweet, 'quoted_tweet'>
-  >()
-  expectTypeOf<
-    NonNullable<Tweet['quoted_tweet']>['self_thread']
-  >().toEqualTypeOf<
-    NonNullable<UpstreamTweet['quoted_tweet']>['self_thread'] | undefined
-  >()
 })
